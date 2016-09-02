f344dc17bf8104f506cb84566525291b398c2464
Lazyman <771623548@qq.com>
2016-09-02 20:07:29 +0800

    Add files via upload

---
+++ IP_Check_Login/IP_INDEX.php
@@ -0,0 +1,12 @@
+﻿<?php
+$reIP=$_SERVER["REMOTE_ADDR"]; 
+$myfile = fopen("218.88.26.135", "r") or die("Unable to open file!");
+$file_info=fread($myfile,filesize("218.88.26.135"));
+fclose($myfile);
+if($file_info=="logined"){
+echo "已登陆！";
+}else{
+echo "未登录！";
+}
+?>
+<html><a href="login_out.php">退出登录</a><br><a href="GetIP.php"返回GetIP.php</a></html>
---
+++ IP_Check_Login/login_out.php
@@ -0,0 +1,11 @@
+﻿<?php
+$reIP=$_SERVER["REMOTE_ADDR"]; 
+echo $reIP; 
+$myfile = fopen($reIP, "w") or die("<red>提示：这里程序出现了一个错误，希望你能帮忙报告给网站的技术员/n将后面的一段文字报告给技术员“GetIPerror”，谢谢！</red>");
+$txt = "unlogin";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
+<html>
+<a href="IP_INDEX.php">GO_BACK!</a>
+</html>
